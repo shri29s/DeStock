fd2886fd022d33782ff0fa8b7f0bb2a59337ed45
yoriichi-69 <shreesaanth@gmail.com>
2025-07-04 10:10:32 +0530

    some server changes - pending commits

--- apps/web/.next/server/server-reference-manifest.js
+++ apps/web/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"s3vHBGUS9SYBRB6fg2lU1jPOE2ZbPAo3xmpYyy5d26o=\"\n}"
